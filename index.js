2e55d1adecfd1db3a3519c6d4e4c99df1c8fa8a3
Aleksa Crveni <sestudent321@gmail.com>
2021-06-02 22:55:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var cron = require('node-cron');
+require('dotenv').config({path: __dirname + '/.env'})
+try{
+let list;
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  list = client.guilds.cache.get("849718699204411453"); 
+  
+});
+var user;
+
+client.on('message',message=>{
+    if(message.author.id == String(process.env.WICK_ID)){
+        list.members.cache.forEach(member => {console.log(member.user.id);if(member.user.id === String(process.env.WICK_ID))
+            {
+                member.user.send("Test").catch(err=>{console.log(err)})
+            }
+        });
+    }
+})
+
+cron.schedule("0 16 * * *",()=>{
+    list.members.cache.forEach(member => {console.log(member.user.id);if(member.user.id === String(process.env.USER_ID))
+        {
+            member.user.send("Wick is 20 years old!").catch(err=>{console.log(err)})
+        }
+    });
+    
+})
+}catch(err){
+
+}
+
+//client.users.cache.get('140843716016472065').send('Test');
+
+client.login(String(process.env.TOKEN));
